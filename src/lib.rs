010646b4108ddcb41a00493e4a6651cc2c564222
DasLixou <einlixou@gmail.com>
2022-10-19 14:53:36 +0200

    remove registry

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,11 @@ mod item;
 mod itemdata;
 mod itemdata_reflection;
 mod itemtype;
-mod registry;
 
 pub use inventory::Inventory;
 pub use item::Item;
 pub use itemdata::ItemData;
 pub use itemtype::ItemType;
-pub use registry::Registry;
 
 #[cfg(feature = "proc")]
 pub use shrub_macros::ItemData;
